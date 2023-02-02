fef268dfc8d3b0c9c3c5b3511c5c354c5341d811
Vladislavius17 <114662177+Vladislavius17@users.noreply.github.com>
2023-02-02 13:31:58 +0100

    Add files via upload

---
+++ 1p/index.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+$a = 2;
+$b = 3;
+
+// PHP_EOL; // przejście dlaej w terminalu
+
+$e = "Tak";
+
+echo str_replace("T", "N", $e);
+echo str_replace("a", "i", $e);
+echo str_replace("k", "e", $e);
+
+pow(3, "tak")
+//dom 5 - 10 funkcji
+min()//tablica
+max()//tablica
+rand()
+round()
+floor()
+ceil()
+abs()
+array_push()
+array_pop()
+array_shift()
+//podzielić na części adres szkoły
